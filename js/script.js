226aa0ccd41e917ba97b4ef1408bea9a875a1ed8
The73756 <94830363+The73756@users.noreply.github.com>
2022-06-16 10:53:31 +0900

    add slider

--- js/script.js
+++ js/script.js
@@ -277,4 +277,55 @@ document.addEventListener('DOMContentLoaded', () => {
 			closeModal();
 		}, 3000);
 	}
+
+	// Slider 
+
+	const slides = document.querySelectorAll('.offer__slide'),
+		prev = document.querySelector('.offer__slider-prev'),
+		next = document.querySelector('.offer__slider-next'),
+		total = document.querySelector('#total'),
+		current = document.querySelector('#current');
+	let slideIndex = 1;
+
+	showSlides(slideIndex);
+
+	if (slides.length < 10) {
+		total.textContent = `0${slides.length}`;
+	} else {
+		total.textContent = slides.length;
+	}
+	
+	function showSlides(n) {
+		if (n > slides.length) {
+			slideIndex = 1;
+		}
+		if (n < 1){
+			slideIndex = slides.length;
+		}
+
+		slides.forEach(el => {
+			el.style.display = 'none';
+		});
+		
+		slides[slideIndex - 1].style.display = 'block';
+
+		if (slideIndex < 10) {
+			current.textContent = `0${slideIndex}`;
+		} else {
+			current.textContent = slideIndex;
+		}
+	}
+
+	function plusSlides(n) {
+		showSlides(slideIndex += n);
+	}
+
+	prev.addEventListener('click', () => {
+		plusSlides(-1);
+	});
+
+	next.addEventListener('click', () => {
+		plusSlides(+1);
+	});
+
 });
