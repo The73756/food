15fe7517ca94f6e64825e3ced2b3aa58923dfe5b
The73756 <94830363+The73756@users.noreply.github.com>
2022-06-17 14:32:12 +0900

    add calculator

--- js/script.js
+++ js/script.js
@@ -402,5 +402,76 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	});
 
+	//Calc
+
+	const result = document.querySelector('.calculating__result span');
+	let sex = 'female', height, weight, age, ratio = '1.375';
+
+	function calcTotal() {  
+		if (!sex || !height || !weight || !age || !ratio) {
+			result.textContent = '____';
+			return;
+		}
+
+		if (sex == 'female') {
+			result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+		} else {
+			result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+		}
+	}
+
+	function getStaticInfo(parentSelector, activeClass) {  
+		const elements = document.querySelectorAll(`${parentSelector} div`);
+
+		elements.forEach(el => {
+			el.addEventListener('click', (e) => {
+				if (e.target.getAttribute('data-ratio')) {
+					ratio = +e.target.getAttribute('data-ratio');
+				} else {
+					sex = e.target.getAttribute('id');
+				}
+	
+				elements.forEach(el => {
+					el.classList.remove(activeClass);
+				});
+				
+				e.target.classList.add(activeClass);		
+				calcTotal();
+			});
+		});
+	}
+
+	function getDynamicInfo(selector) {  
+		const input = document.querySelector(selector);
+
+		input.addEventListener('input', () => {
+
+			if (input.value.match (/\D/g)) {
+				input.style.border = '1px solid red';
+			} else {
+				input.style.border = 'none';
+			}
+
+			switch(input.getAttribute('id')) {
+				case 'height': 
+					height = +input.value;
+					break;
+				case 'weight': 
+				weight = +input.value;
+					break;
+				case 'age': 
+				age = +input.value;
+					break;
+			}
+			calcTotal();
+		});
+	}
+	
+	getDynamicInfo('#height');
+	getDynamicInfo('#weight');
+	getDynamicInfo('#age');
+
+	getStaticInfo('#gender', 'calculating__choose-item_active');
+	getStaticInfo('#ratio', 'calculating__choose-item_active');
 });
 
